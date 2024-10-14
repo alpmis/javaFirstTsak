f6aa14cf6c7ffd3adc7595d53d01ad3b08398fef
alpmis <ajoldasov013@gmail.com>
2024-10-14 08:55:34 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+// Begin1
+// let a = +prompt('Kvadratning taomoni a = ')
+// console.log("Kvadratning taomoni a ni kiriting: " + a + "sm")
+// let P;
+// P = 4*a;
+// console.log("Kvadratning perimetri: " + P + " sm ga teng")
+
+// Begin2
+// let a = +prompt('Kvadratning taomoni a = ')
+// console.log("Kvadratning taomoni a ni kiriting: " + a + "sm")
+// let S;
+// S = a*a;
+// console.log("Kvadratning maydoni: " + S + " sm^2 ga teng");
+
+// Begin3
+// let a = +prompt('Togri tortburchakning taomoni a ni kiriting: ')
+// console.log("Togri tortburchakning taomoni a = " + a + "sm")
+// let b = +prompt('Togri tortburchakning taomoni b ni kiriting: ')
+// console.log("Togri tortburchakning taomoni b = " + b + "sm")
+// let S;
+// let P;
+// S=a*b;
+// P=2*(a+b);
+// console.log("Togri tortburchakning maydoni: " + S + " sm^2 ga teng");
+// console.log("Togri tortburchakning peimetri: " + P + " sm ga teng");
+
+// Begin4
+// let d = +prompt('Aylananing diametri d ni kiriting: ')
+// console.log("Aylananing diametri d = " + d + " sm")
+// const p=3.14;
+// console.log("p = 3.14")
+// let L;
+// L=p*d;
+// console.log("Aylananing uzunligi L = " + L + " sm")
+
+// Begin 5
+// let a = +prompt("Kubning yon tomoni a ni kiriting: ")
+// console.log("Aylananing yon tomoni a = " + a + " sm")
+// let V;
+// V=a*a*a;
+// let S;
+// S=2*6*a*a;
+// console.log("Aylananing Hajmi V = "+ V + " sm^3, Aylananing Tola sirti S = "+ S +" sm^2")
+
+//Begin 6
+// let a = +prompt("Paralellepipedning tomoni a ni kiriting: ")
+// console.log("Paralellepipedning tomoni a = " + a + " sm")
+// let b = +prompt("Paralellepipedning tomoni b ni kiriting: ")
+// console.log("Paralellepipedning tomoni b = " + b + " sm")
+// let c = +prompt("Paralellepipedning tomoni c ni kiriting: ")
+// console.log("Paralellepipedning tomoni c = " + c + " sm")
+// let S;
+// S=2*(a*b+b*c+a*c);
+// console.log("Paralellepipedning tola sirti S = " + S + " sm^2")
+
+// Begin7
+// let r = +prompt('Doiranining radiusi R ni kiriting: ')
+// console.log("Doiranining radiusi R = "+ r + " sm")
+// const p=3.14;
+// console.log("p = 3.14")
+// let L;
+// L=2*p*r
+// let S;
+// S=p*r*r;
+// console.log("Doiraning uzunligi L = "+ L + " sm, Doiraning yuzasi S = " + S +" sm^2")
+
+// Begin 8
+// let a = +prompt("a ozgaruvchiga qiymat bering:")
+// console.log("a = "+ a)
+// let b = +prompt("b ozgaruvchiga qiymat bering:")
+// console.log("b = "+ b)
+// let P;
+// P=(a+b)/2;
+// console.log("a va b sonlarining orta aripmetigi P = "+P+" ga teng!")
+
+// Begin9
+// let a = +prompt("a ozgaruvchiga musbat qiymat bering: (a > = 0)")
+// console.log("a = "+ a)
+// let b = +prompt("b ozgaruvchiga musbat qiymat bering: (b > = 0)")
+// console.log("b = "+ b)
+// let S;
+// S=Math.sqrt(a*b);
+// console.log("a va b sonlarining orta aripmetigi S = "+S+" ga teng!")
+
+// Begin12
+// let a = +prompt('Togri uchburchakning a katetiga qiymat bering: ')
+// console.log("Togri uchburchakning kateti: a = "+a+" sm,")
+// let b = +prompt('Togri uchburchakning b katetiga qiymat bering: ')
+// console.log("Togri uchburchakning kateti: b = "+b+" sm,")
+// let c=Math.sqrt(a*a+b*b)
+// let P=a+b+c;
+// console.log("Togri uchburchakning kateti: c = "+c+" sm, Togri uchburchakning perimetri: P = "+P+" sm")
